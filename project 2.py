6e21b2fde27b4e7febed177d6a4171df8461821a
Nikita-Ud-dev <nikitaudovik777@gmail.com>
2025-06-17 15:22:55 +0300

    homework2.1 fix

--- project 2.py
+++ project 2.py
@@ -1,16 +1,6 @@
 
 user_input = int(input("Введіть чотирьох значне число:"))
 
-if user_input < -1:
-    print("Введіть чотирьох значне число вище -1")
-else:
-    print("Так все правильно")
-
-if user_input > 9999:
-    print("Введіть чотирьох значне число нижче 10000")
-else:
-    print("Так все правильно")
-
 number0 = (user_input // 1000)
 number1 = (user_input // 100)
 number2 = (user_input // 10)
