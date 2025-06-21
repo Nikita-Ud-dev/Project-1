b3ceaa444a7cfc3546ac3ecf3c6a4049efdea3c9
Nikita-Ud-dev <nikitaudovik777@gmail.com>
2025-06-21 21:46:06 +0300

    homework3.2 fix2

--- project 6.py
+++ project 6.py
@@ -3,19 +3,20 @@ l2 = [5]
 l3 = []
 l4 = [30, 5, 7, 15, 40]
 
-l3.append(1)
+l.insert(0,111)
+l2.insert(0,111)
+l3.insert(0,111)
+l4.insert(0,111)
 
-x1 = l.pop(-1)
-x2 = l2.pop(-1)
-x3 = l3.pop(-1)
-x4 = l4.pop(-1)
+l.insert(0, l.pop(-1))
+l2.insert(0, l2.pop(-1))
+l3.insert(0, l3.pop(-1))
+l4.insert(0, l4.pop(-1))
 
-l.insert(0, x1)
-l2.insert(0, x2)
-l3.insert(0, x3)
-l4.insert(0, x4)
-
-del l3[-1]
+l.remove(111)
+l2.remove(111)
+l3.remove(111)
+l4.remove(111)
 
 print(l)
 print(l2)
