7d78fb2cd3065cfc1501e8191558d43a90416dbe
Nikita-Ud-dev <nikitaudovik777@gmail.com>
2025-06-29 17:50:02 +0300

    homework5.1 fix

--- project 11.py
+++ project 11.py
@@ -1,6 +1,15 @@
+import keyword
 
 user_input = str(input("Введіть ім'я змінної:"))
 lst1 = user_input.split(" ")
+
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+punctuation0 =  ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-",
+                 ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\'", "]",
+                 "^", "`", "{", "|", "}", "~"]
+
+punctuation2 = ["__", "___", "____", "_____"]
 
 for space in user_input:
     if space == " ":
@@ -10,184 +19,30 @@ for space in user_input:
     else:
         continue
 ####################################
-for punctuation in user_input: # string.punctuation
-    if punctuation == "!":
-        print(False)
-    elif punctuation == '"':
-        print(False)
-    elif punctuation == "#":
-        print(False)
-    elif punctuation == "$":
-        print(False)
-    elif punctuation == "%":
-        print(False)
-    elif punctuation == "&":
-        print(False)
-    elif punctuation == "'":
-        print(False)
-    elif punctuation == "(":
-        print(False)
-    elif punctuation == ")":
-        print(False)
-    elif punctuation == "*":
-        print(False)
-    elif punctuation == "+":
-        print(False)
-    elif punctuation == ",":
-        print(False)
-    elif punctuation == "-":
-        print(False)
-    elif punctuation == ".":
-        print(False)
-    elif punctuation == "/":
-        print(False)
-    elif punctuation == ":":
-        print(False)
-    elif punctuation == ";":
-        print(False)
-    elif punctuation == "<":
-        print(False)
-    elif punctuation == "=":
-        print(False)
-    elif punctuation == ">":
-        print(False)
-    elif punctuation == "?":
-        print(False)
-    elif punctuation == "@":
-        print(False)
-    elif punctuation == "[":
-        print(False)
-    elif punctuation == "\"":
-        print(False)
-    elif punctuation == "]":
-        print(False)
-    elif punctuation == "^":
-        print(False)
-    elif punctuation == "`":
-        print(False)
-    elif punctuation == "{":
-        print(False)
-    elif punctuation == "|":
-        print(False)
-    elif punctuation == "}":
-        print(False)
-    elif punctuation == "~":
+for char in user_input: # string.punctuation "Не беремо виняток: '_'"
+    if char in punctuation0:
         print(False)
     else:
         continue
 ####################################
-for punctuation1 in lst1:
-    if punctuation1 == "__":
-        print(False)
-    elif punctuation1 == "___":
-        print(False)
-    elif punctuation1 == "____":
-        print(False)
-    elif punctuation1 == "_____":
+for punctuation1 in lst1: # "'__', '___', '____', '_____'"
+    if punctuation1 in punctuation2:
         print(False)
     else:
         continue
 ####################################
-for keyword in  lst1: #keyword.kwlist
-    if keyword == "False":
+for keyword1 in lst1: #keyword.kwlist
+    if keyword1 in keyword.kwlist:
         print(False)
-    elif keyword == "None":
+    elif keyword1 == "match":
         print(False)
-    elif keyword == "True":
-        print(False)
-    elif keyword == "and":
-        print(False)
-    elif keyword == "as":
-        print(False)
-    elif keyword == "assert":
-        print(False)
-    elif keyword == "async":
-        print(False)
-    elif keyword == "await":
-        print(False)
-    elif keyword == "break":
-        print(False)
-    elif keyword == "class":
-        print(False)
-    elif keyword == "continue":
-        print(False)
-    elif keyword == "def":
-        print(False)
-    elif keyword == "del":
-        print(False)
-    elif keyword == "elif":
-        print(False)
-    elif keyword == "else":
-        print(False)
-    elif keyword == "except":
-        print(False)
-    elif keyword == "finally":
-        print(False)
-    elif keyword == "for":
-        print(False)
-    elif keyword == "from":
-        print(False)
-    elif keyword == "global":
-        print(False)
-    elif keyword == "if":
-        print(False)
-    elif keyword == "import":
-        print(False)
-    elif keyword == "in":
-        print(False)
-    elif keyword == "is":
-        print(False)
-    elif keyword == "lambda":
-        print(False)
-    elif keyword == "nonlocal":
-        print(False)
-    elif keyword == "not":
-        print(False)
-    elif keyword == "or":
-        print(False)
-    elif keyword == "pass":
-        print(False)
-    elif keyword == "raise":
-        print(False)
-    elif keyword == "return":
-        print(False)
-    elif keyword == "try":
-        print(False)
-    elif keyword == "while":
-        print(False)
-    elif keyword == "with":
-        print(False)
-    elif keyword == "yield":
-        print(False)
-    elif keyword == "case":
-        print(False)
-    elif keyword == "match":
-        print(False)
-    elif keyword == "type":
+    elif keyword1 == "type":
         print(False)
     else:
         continue
 ####################################
-for number in lst1: #tuple("0""1""2""3""4""5""6""7""8""9")
-    if number[0] == "0":
-        print(False)
-    elif number[0] == "1":
-        print(False)
-    elif number[0] == "2":
-        print(False)
-    elif number[0] == "3":
-        print(False)
-    elif number[0] == "4":
-        print(False)
-    elif number[0] == "5":
-        print(False)
-    elif number[0] == "6":
-        print(False)
-    elif number[0] == "7":
-        print(False)
-    elif number[0] == "8":
-        print(False)
-    elif number[0] == "9":
+for number in lst1: # numbers
+    if number[0] in numbers:
         print(False)
     else:
         break
@@ -196,7 +51,7 @@ low = user_input.lower()
 user_input2 = "".join(user_input)
 for letter in lst1:
     if not low == letter:
-        print(False, 3)
+        print(False)
     else:
         print(True)
         continue
