b54f56d2bad5f232df5487a7ce1864835504791c
Nikita-Ud-dev <nikitaudovik777@gmail.com>
2025-06-29 18:17:25 +0300

    homework5.1 fix

--- project 11.py
+++ project 11.py
@@ -14,44 +14,54 @@ punctuation2 = ["__", "___", "____", "_____"]
 for space in user_input:
     if space == " ":
         print(False)
+        break
     elif space == "":
         print(False)
+        break
     else:
-        continue
+        break
 ####################################
 for char in user_input: # string.punctuation "Не беремо виняток: '_'"
     if char in punctuation0:
         print(False)
+        break
     else:
         continue
 ####################################
 for punctuation1 in lst1: # "'__', '___', '____', '_____'"
     if punctuation1 in punctuation2:
         print(False)
+        break
     else:
         continue
 ####################################
 for keyword1 in lst1: #keyword.kwlist
     if keyword1 in keyword.kwlist:
         print(False)
+        break
     elif keyword1 == "match":
         print(False)
+        break
     elif keyword1 == "type":
         print(False)
+        break
     else:
         continue
 ####################################
 for number in lst1: # numbers
     if number[0] in numbers:
         print(False)
+        break
     else:
         break
 ####################################
 low = user_input.lower()
+
 user_input2 = "".join(user_input)
-for letter in lst1:
+for letter in user_input:
     if not low == letter:
         print(False)
+        break
     else:
         print(True)
-        continue
+        break
