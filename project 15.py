64c37408ae559df30d170527e1fb20501727066b
Nikita-Ud-dev <nikitaudovik777@gmail.com>
2025-07-05 16:11:38 +0300

    homework6.2 fix

--- project 15.py
+++ project 15.py
@@ -22,7 +22,17 @@ while True:
         minute1 = hour2 // minute
         minute2 = hour2 % minute
 
-        user_input2 = f"{day1} Днів {str(hour1).zfill(2)}:{str(minute1).zfill(2)}:{str(minute2).zfill(2)}"
+        day_declination = ""
+        if 11 <= day1 % 100 <= 14:
+            day_declination = "Днів"
+        elif 2 <= day1 % 10 <= 4:
+            day_declination = "Дні"
+        elif 1 == day1 % 10:
+            day_declination = "День"
+        else:
+            day_declination = "Днів"
+
+        user_input2 = f"{day1} {day_declination} {str(hour1).zfill(2)}:{str(minute1).zfill(2)}:{str(minute2).zfill(2)}"
         print(user_input2)
 
         break
