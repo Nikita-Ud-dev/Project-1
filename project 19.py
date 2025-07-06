34df7d0cf7cfc8e029c7b43dffd3d807e4dd63f1
Nikita-Ud-dev <nikitaudovik777@gmail.com>
2025-07-06 21:46:49 +0300

    homework7.3 fix

--- project 19.py
+++ project 19.py
@@ -1,16 +1,13 @@
-def second_index(text: str, some_str: str ):
-        target = text.find(some_str)
-        target1 = text.find(some_str,target + 1)
-        if target1 == -1:
-                return None
-        return target1
+def second_index(text1: str, some_str1: str):
+    target = text1.find(some_str1)
+    target1 = text1.find(some_str1, target + 1)
+    if target1 == -1:
+        return None
+    return target1
+
 
 text = input("Введіть рядок:")
 some_str = input("Введіть символи які хочете знайти:")
 
 print(second_index(text, some_str))
 print(second_index("Hello, hello", "lo"))
-
-
-
-
