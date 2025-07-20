fd2af2ca5623fc5efcec9ef95d34df105b86b18a
Nikita-Ud-dev <nikitaudovik777@gmail.com>
2025-07-20 19:04:00 +0300

    homework11.1 fix

--- project 29.py
+++ project 29.py
@@ -1,16 +1,14 @@
-def numbers(digit: int):
-    if digit < 2:
-        return False
-    for digit1 in range(2, int(digit ** 0.5) + 1):
-        if digit % digit1 == 0:
-            return False
-    return True
+import math
 
 def prime_generator(end: int):
-    for number in range(2, end + 1):
-        if numbers(number):
-            yield number
-
+    for number1 in range(2, end + 1):
+        result = math.sqrt(number1)
+        for number in range(2, int(result) + 1):
+            x = number1 % number
+            if x == 0:
+                break
+        else:
+            yield number1
 
 from inspect import isgenerator
 
@@ -21,5 +19,12 @@ print(list(prime_generator(15))) #== [2, 3, 5, 7, 11, 13], 'Test2'
 print(list(prime_generator(29))) #== [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
 
 
+# def numbers(digit: int):
+#     if digit < 2:
+#         return False
+#     for digit1 in range(2, int(digit ** 0.5) + 1):
+#         if digit % digit1 == 0:
+#             return False
+#     return True
 
-
+# if numbers(number):
