5880bc8f5d62beb8b43173b1adf214eb4e96d2b7
Nikita-Ud-dev <nikitaudovik777@gmail.com>
2025-08-03 19:44:57 +0300

    homework: 14.2

---
+++ package_1/classes.py
@@ -0,0 +1,88 @@
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return (f"First_name: {self.first_name}\n"
+                f"Last_name: {self.last_name}\n"
+                f"Age: {self.age}\n"
+                f"Gender: {self.gender}")
+
+class Student(Human):
+    number_student = 0
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        Student.number_student += 1
+        self.number_student1 = Student.number_student
+
+    def __eq__(self, other):
+        if not isinstance(other, Student): # Якщо об'єкт не являється екземпляром студента то Фолс
+            return False
+        return (self.first_name == other.first_name and # Порівнюються атребути щоб розуміти об'єкт дублікат студенту чи ні
+                self.last_name == other.last_name and
+                self.record_book == other.record_book)
+
+
+        # elif self.first_name :
+        #     return self.first_name == other.first_name
+        # elif self.last_name == other.last_name:
+        #     return self.last_name == other.last_name
+        # elif self.age == other.age:
+        #     return self.age == other.age
+        # elif self.gender == other.gender:
+        #     return self.gender == other.gender
+        # elif self.record_book == other.record_book:
+        #     return self.record_book == other.record_book
+        # return None
+
+
+    def __str__(self):
+        return (f"Student {self.number_student1}\n"
+                f"First_name: {self.first_name}\n"
+                f"Last_name: {self.last_name}\n"
+                f"Age: {self.age}\n"
+                f"Gender: {self.gender}\n"
+                f"Record_book: {self.record_book}\n")
+
+
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = [] # Замінив сет на лист щоб зберегти порядок, і використовуючи тип данних слист я рузумію що метод hash мені не потрібен
+
+
+    def add_student(self, student):
+        if student not in self.group:
+            self.group.append(student)
+        else:
+            print("Виявлено дублікат студенту\n")
+
+    def delete_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return self.group.remove(student)
+        return None
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = "\n"
+        for student in self.group:
+            all_students = all_students + str(student)
+            all_students = all_students + "\n"
+        return f'Number:{self.number}\n{all_students}'
